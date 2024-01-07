9b9ad6592e5ec04ca8226e2f33bc3d9bdd7b77cc
ocopywriter <endlessmidia@gmail.com>
2024-01-07 13:38:59 -0300

    Projection of author data with DTO

--- src/main/java/com/apyetro/projetomongo/config/Instatiation.java
+++ src/main/java/com/apyetro/projetomongo/config/Instatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.apyetro.projetomongo.domain.Post;
 import com.apyetro.projetomongo.domain.User;
+import com.apyetro.projetomongo.dto.AuthorDTO;
 import com.apyetro.projetomongo.repository.PostRepository;
 import com.apyetro.projetomongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instatiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
