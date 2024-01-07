224d6cb2d94561240ad02419e5dc09d07213c79e
ocopywriter <endlessmidia@gmail.com>
2024-01-07 14:35:46 -0300

    simple query - query methods

--- src/main/java/com/apyetro/projetomongo/resources/PostResource.java
+++ src/main/java/com/apyetro/projetomongo/resources/PostResource.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.apyetro.projetomongo.domain.Post;
 import com.apyetro.projetomongo.domain.User;
 import com.apyetro.projetomongo.dto.UserDTO;
+import com.apyetro.projetomongo.resources.util.URL;
 import com.apyetro.projetomongo.services.PostService;
 
 @RestController
@@ -30,6 +32,13 @@ public class PostResource {
 		Post obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);	
+		return ResponseEntity.ok().body(list);
 	}
 
 }
