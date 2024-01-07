72238bf0a941e54ce456975382052aba21f9bd9b
ocopywriter <endlessmidia@gmail.com>
2024-01-07 14:01:58 -0300

    Post by Id

---
+++ src/main/java/com/apyetro/projetomongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.apyetro.projetomongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.apyetro.projetomongo.domain.Post;
+import com.apyetro.projetomongo.repository.PostRepository;
+import com.apyetro.projetomongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
