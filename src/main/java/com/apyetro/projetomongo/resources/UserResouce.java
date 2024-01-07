811ca89c4d0f5581a7ff9a232bf05b517406aef6
ocopywriter <endlessmidia@gmail.com>
2024-01-07 13:56:48 -0300

    Endpoint to retrive user posts

--- src/main/java/com/apyetro/projetomongo/resources/UserResouce.java
+++ src/main/java/com/apyetro/projetomongo/resources/UserResouce.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.apyetro.projetomongo.domain.Post;
 import com.apyetro.projetomongo.domain.User;
 import com.apyetro.projetomongo.dto.UserDTO;
 import com.apyetro.projetomongo.services.UserService;
@@ -37,6 +38,7 @@ public class UserResouce {
 	    UserDTO userDTO = new UserDTO(obj);
 	    return ResponseEntity.ok().body(userDTO);
 	}
+	
 	@RequestMapping( method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
@@ -57,7 +59,13 @@ public class UserResouce {
 		obj.setId(id);
 	    obj = service.update(obj);
 	    return ResponseEntity.noContent().build();  
-	}			
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+	    User obj = service.findById(id);	    
+	    return ResponseEntity.ok().body(obj.getPost());
+	}
 
 }
 
