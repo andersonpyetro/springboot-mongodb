3c9c1918a4acaacf2f0c7e6849af2328c081d0ce
ocopywriter <endlessmidia@gmail.com>
2024-01-07 12:33:20 -0300

    User insert with POST

--- src/main/java/com/apyetro/projetomongo/resources/UserResouce.java
+++ src/main/java/com/apyetro/projetomongo/resources/UserResouce.java
@@ -1,15 +1,17 @@
 package com.apyetro.projetomongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.apyetro.projetomongo.domain.User;
 import com.apyetro.projetomongo.dto.UserDTO;
@@ -35,6 +37,15 @@ public class UserResouce {
 	    UserDTO userDTO = new UserDTO(obj);
 	    return ResponseEntity.ok().body(userDTO);
 	}
+	@RequestMapping( method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+	    obj = service.insert(obj);
+	    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	    return ResponseEntity.created(uri).build(); 		
+	}   
+	
+
 }
 
 
